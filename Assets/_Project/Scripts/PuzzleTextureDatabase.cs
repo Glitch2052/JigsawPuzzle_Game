e5f97abcabd156474cd14133cfa870b9afb6c5c4
Kamlesh <kamleshsirvi46@gmail.com>
2024-10-06 17:44:06 +0530

    script added

---
+++ Assets/_Project/Scripts/PuzzleTextureDatabase.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Puzzle Data/Puzzle Texture Database",fileName = "puzzleTexture Database")]
+public class PuzzleTextureDatabase : ScriptableObject
+{
+    
+}
