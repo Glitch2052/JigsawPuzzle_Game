3032db8d76268876cd986e94214572cdba7ba833
Kamlesh <kamleshsirvi46@gmail.com>
2024-10-06 16:12:57 +0530

    new textures added

---
+++ Assets/_Project/Scripts/UIManager.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField] private UIDocument mainMenuDoc;
+    
+    private void Start()
+    {
+        
+    }
+}
